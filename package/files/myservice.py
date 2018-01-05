e8a1f602655173914d9f617b1510fa3fd1fc07b0
Lev Bronshtein <lbronshtein@bloomberg.net>
2018-01-05 18:19:46 -0500

    working in VM cluster

--- package/files/myservice.py
+++ package/files/myservice.py
@@ -3,11 +3,11 @@ import socket
 
 if __name__ == "__main__":
   tcp_interface = ''
-  tcp_port = sys.argv[1]
-  listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-  listen_socket.bind(tcp_interface, tcp_port)
+  tcp_port = int(sys.argv[1])
+  listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  listen_socket.bind((tcp_interface, tcp_port))
   listen_socket.listen(1)
-  conn, addr = listen_scoket.accept()
+  conn, addr = listen_socket.accept()
   print 'Connected by', addr
   while 1:
     data = conn.recv(1024)
--- package/scripts/myservice.py
+++ package/scripts/myservice.py
@@ -1,23 +1,3 @@
-#!/usr/bin/env python
-"""
-Licensed to the Apache Software Foundation (ASF) under one
-or more contributor license agreements.  See the NOTICE file
-distributed with this work for additional information
-regarding copyright ownership.  The ASF licenses this file
-to you under the Apache License, Version 2.0 (the
-"License"); you may not use this file except in compliance
-with the License.  You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-"""
-
 import sys
 from resource_management import *
 
@@ -34,7 +14,8 @@ class MyService(Script):
     import params
     env.set_params(params)
     self.configure(env)
-    process_cmd = format("python {app_root}/myservice.tsk --port={port}")
+    print format("APP_ROOT: {app_root}")
+    process_cmd = format("/usr/bin/python {app_root}/mysvc.py {port}")
 
     Execute(process_cmd,
         logoutput=False,
